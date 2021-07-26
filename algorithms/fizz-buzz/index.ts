8eec78c68d0d715fec2bae2ed854efbcf8b6a69e
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-07-26 10:26:27 -0300

    feat(fizzbuzz): fizzbuzz refactor empty str

--- algorithms/fizz-buzz/index.ts
+++ algorithms/fizz-buzz/index.ts
@@ -1,7 +1,10 @@
 const fizzbuzz = (entry: number): string => {
-  const fizz = entry % 3 === 0 ? 'fizz' : '';
-  const buzz = entry % 5 === 0 ? 'buzz' : '';
-  const response = `${fizz}${buzz}` !== '' ? `${fizz}${buzz}` : 'invalid entry';
+  const EMPTY = '';
+  const fizz = entry % 3 === 0 ? 'fizz' : EMPTY;
+  const buzz = entry % 5 === 0 ? 'buzz' : EMPTY;
+
+  const response =
+    `${fizz}${buzz}` !== EMPTY ? `${fizz}${buzz}` : 'invalid entry';
 
   return response;
 };
