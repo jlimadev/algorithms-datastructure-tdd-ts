5239e73b125f1629a62b3974143abea4d92b3542
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-08-17 18:39:35 -0300

    feat(algorithms): reverse string using for loops

--- algorithms/reverse-string/index.spec.ts
+++ algorithms/reverse-string/index.spec.ts
@@ -1,9 +1,15 @@
-import { reverseStringUsingArray } from '.';
+import { reverseStringUsingArray, revereStringUsingForLoop } from '.';
 
 describe('reverse string tests', () => {
-  it.only('Should reverse the string successfully using array', () => {
+  it('Should reverse the string successfully using array', () => {
     expect(reverseStringUsingArray('hello')).toBe('olleh');
     expect(reverseStringUsingArray('jonathan')).toBe('nahtanoj');
     expect(reverseStringUsingArray('nalo')).toBe('olan');
+  });
+
+  it('Should reverse the string successfully using for loop', () => {
+    expect(revereStringUsingForLoop('hello')).toBe('olleh');
+    expect(revereStringUsingForLoop('jonathan')).toBe('nahtanoj');
+    expect(revereStringUsingForLoop('nalo')).toBe('olan');
   });
 });
--- algorithms/reverse-string/index.ts
+++ algorithms/reverse-string/index.ts
@@ -2,4 +2,12 @@ const reverseStringUsingArray = (str: string): string => {
   return Array.from(str).reverse().join('');
 };
 
-export { reverseStringUsingArray };
+const revereStringUsingForLoop = (str: string): string => {
+  const data = [];
+  for (let i = str.length; i >= 0; i--) {
+    data.push(str[i]);
+  }
+  return data.join('');
+};
+
+export { reverseStringUsingArray, revereStringUsingForLoop };
