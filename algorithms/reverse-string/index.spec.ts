1cbe133216b0d01f646db7bd62376b78937df56a
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-08-17 19:19:23 -0300

    Revert "feat(algorithms): refactor reverse string tests"
    This reverts commit df4637c3

--- algorithms/reverse-string/index.spec.ts
+++ algorithms/reverse-string/index.spec.ts
@@ -4,49 +4,22 @@ import {
   revereStringUsingReduce,
 } from '.';
 
-const data = {
-  firstString: 'hello',
-  firstStringExpectedResult: 'olleh',
-  secondString: 'jonathan',
-  secondStringExpectedResult: 'nahtanoj',
-  thirdString: 'nalo',
-  thirdStringExpectedResult: 'olan',
-};
-
 describe('reverse string tests', () => {
   it('Should reverse the string successfully using array', () => {
-    expect(reverseStringUsingArray(data.firstString)).toBe(
-      data.firstStringExpectedResult,
-    );
-    expect(reverseStringUsingArray(data.secondString)).toBe(
-      data.secondStringExpectedResult,
-    );
-    expect(reverseStringUsingArray(data.thirdString)).toBe(
-      data.thirdStringExpectedResult,
-    );
+    expect(reverseStringUsingArray('hello')).toBe('olleh');
+    expect(reverseStringUsingArray('jonathan')).toBe('nahtanoj');
+    expect(reverseStringUsingArray('nalo')).toBe('olan');
   });
 
   it('Should reverse the string successfully using for loop', () => {
-    expect(revereStringUsingForLoop(data.firstString)).toBe(
-      data.firstStringExpectedResult,
-    );
-    expect(revereStringUsingForLoop(data.secondString)).toBe(
-      data.secondStringExpectedResult,
-    );
-    expect(revereStringUsingForLoop(data.thirdString)).toBe(
-      data.thirdStringExpectedResult,
-    );
+    expect(revereStringUsingForLoop('hello')).toBe('olleh');
+    expect(revereStringUsingForLoop('jonathan')).toBe('nahtanoj');
+    expect(revereStringUsingForLoop('nalo')).toBe('olan');
   });
 
   it('Should reverse the string successfully using reduce', () => {
-    expect(revereStringUsingReduce(data.firstString)).toBe(
-      data.firstStringExpectedResult,
-    );
-    expect(revereStringUsingReduce(data.secondString)).toBe(
-      data.secondStringExpectedResult,
-    );
-    expect(revereStringUsingReduce(data.thirdString)).toBe(
-      data.thirdStringExpectedResult,
-    );
+    expect(revereStringUsingReduce('hello')).toBe('olleh');
+    expect(revereStringUsingReduce('jonathan')).toBe('nahtanoj');
+    expect(revereStringUsingReduce('nalo')).toBe('olan');
   });
 });
