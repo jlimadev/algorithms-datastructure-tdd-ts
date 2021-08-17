3ba5acc3b3600ce7a2045cf4f9105911299bc170
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-08-17 19:42:17 -0300

    feat(algorithms): check palindrome algorithm using array.every

--- algorithms/palindrome/index.spec.ts
+++ algorithms/palindrome/index.spec.ts
@@ -1,4 +1,4 @@
-import { checkPalindrome } from '.';
+import { checkPalindrome, checkPalindromeUsingEvery } from '.';
 
 describe('palindrome tests', () => {
   it('Should return true if word is palindrome', () => {
@@ -11,5 +11,19 @@ describe('palindrome tests', () => {
     expect(checkPalindrome('pato')).toBe(false);
     expect(checkPalindrome('bear')).toBe(false);
     expect(checkPalindrome('jonathan')).toBe(false);
+  });
+});
+
+describe('palindrome tests using every', () => {
+  it('Should return true if word is palindrome', () => {
+    expect(checkPalindromeUsingEvery('abba')).toBe(true);
+    expect(checkPalindromeUsingEvery('madam')).toBe(true);
+    expect(checkPalindromeUsingEvery('tenet')).toBe(true);
+  });
+
+  it('Should return false if word is not palindrome', () => {
+    expect(checkPalindromeUsingEvery('pato')).toBe(false);
+    expect(checkPalindromeUsingEvery('bear')).toBe(false);
+    expect(checkPalindromeUsingEvery('jonathan')).toBe(false);
   });
 });
--- algorithms/palindrome/index.ts
+++ algorithms/palindrome/index.ts
@@ -3,4 +3,14 @@ const checkPalindrome = (str: string): boolean => {
   return str === reversed;
 };
 
-export { checkPalindrome };
+// this is not the best solution, but it is another way to solve
+const checkPalindromeUsingEvery = (str: string): boolean => {
+  return Array.from(str).every((char: string, index: number) => {
+    // first iteration - compare current char with the last
+    // second iteration - compare current char with the second last
+    // n interaction - compare current char with the n last (or itself)
+    return char === str[str.length - index - 1];
+  });
+};
+
+export { checkPalindrome, checkPalindromeUsingEvery };
