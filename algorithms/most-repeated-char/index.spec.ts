0377863bcea70af12ee779473559264d32c0d00b
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-08-18 13:22:35 -0300

    feat(algorithms): most repeated char in a string, second solution

--- algorithms/most-repeated-char/index.spec.ts
+++ algorithms/most-repeated-char/index.spec.ts
@@ -1,4 +1,4 @@
-import { mostRepeatedChar } from '.';
+import { mostRepeatedChar, mostRepeatedCharV2 } from '.';
 
 describe('reverse string tests', () => {
   describe('first solution', () => {
@@ -7,6 +7,15 @@ describe('reverse string tests', () => {
       expect(mostRepeatedChar('banana')).toBe('a');
       expect(mostRepeatedChar('hey ho lets go')).toBe(' ');
       expect(mostRepeatedChar('banana 111142')).toBe('1');
+    });
+  });
+
+  describe('second solution', () => {
+    it('Should return the most repeated char in a string', () => {
+      expect(mostRepeatedCharV2('apple')).toBe('p');
+      expect(mostRepeatedCharV2('banana')).toBe('a');
+      expect(mostRepeatedCharV2('hey ho lets go')).toBe(' ');
+      expect(mostRepeatedCharV2('banana 111142')).toBe('1');
     });
   });
 });
--- algorithms/most-repeated-char/index.ts
+++ algorithms/most-repeated-char/index.ts
@@ -1,5 +1,6 @@
+type CharMap = { [anyStr: string]: number }; // Index Signature (Typescript Only)
+
 const mostRepeatedChar = (str: string): string => {
-  type CharMap = { [anyStr: string]: number }; // Index Signature (Typescript Only)
   const charMap: CharMap = {};
   let maxCount = 0;
   let mostRepeated = '';
@@ -18,4 +19,27 @@ const mostRepeatedChar = (str: string): string => {
   return mostRepeated;
 };
 
-export { mostRepeatedChar };
+// Pretty much the same logic, but using "Object.entries" to iteration instead of "for in"
+const mostRepeatedCharV2 = (str: string): string => {
+  const charMap: CharMap = {};
+  let maxCount = 0;
+  let mostRepeated = '';
+
+  str.split('').forEach(char => {
+    charMap[char] = charMap[char] + 1 || 1;
+  });
+
+  Object.entries(charMap).forEach(entry => {
+    const key = entry[0];
+    const value = entry[1];
+
+    if (value > maxCount) {
+      maxCount = value;
+      mostRepeated = key;
+    }
+  });
+
+  return mostRepeated;
+};
+
+export { mostRepeatedChar, mostRepeatedCharV2 };
