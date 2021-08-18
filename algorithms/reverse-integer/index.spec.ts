ef8229d872be4c6a1f19a3c6f1e78f9b254f399b
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-08-18 09:38:22 -0300

    feat(algorithms): reverse integer cleaner solution

--- algorithms/reverse-integer/index.spec.ts
+++ algorithms/reverse-integer/index.spec.ts
@@ -1,11 +1,39 @@
-import { reverseInt } from '.';
+import { reverseIntFirstSolution, reverseIntSecondSolution } from '.';
 
 describe('reverse string tests', () => {
-  it('Should reverse integer number', () => {
-    expect(reverseInt(-25)).toBe(-52);
-    expect(reverseInt(10)).toBe(1);
-    expect(reverseInt(854)).toBe(458);
-    expect(reverseInt(-40)).toBe(-4);
-    expect(reverseInt(0)).toBe(0);
+  describe('first solution', () => {
+    it('Should reverse integer positive numbers', () => {
+      expect(reverseIntFirstSolution(10)).toBe(1);
+      expect(reverseIntFirstSolution(854)).toBe(458);
+      expect(reverseIntFirstSolution(4500)).toBe(54);
+    });
+
+    it('Should reverse integer negative numbers', () => {
+      expect(reverseIntFirstSolution(-25)).toBe(-52);
+      expect(reverseIntFirstSolution(-40)).toBe(-4);
+      expect(reverseIntFirstSolution(-8533)).toBe(-3358);
+    });
+
+    it('Should return 0 when the input is 0', () => {
+      expect(reverseIntFirstSolution(0)).toBe(0);
+    });
+  });
+
+  describe('second solution', () => {
+    it('Should reverse integer positive numbers', () => {
+      expect(reverseIntSecondSolution(10)).toBe(1);
+      expect(reverseIntSecondSolution(854)).toBe(458);
+      expect(reverseIntSecondSolution(4500)).toBe(54);
+    });
+
+    it('Should reverse integer negative numbers', () => {
+      expect(reverseIntSecondSolution(-25)).toBe(-52);
+      expect(reverseIntSecondSolution(-40)).toBe(-4);
+      expect(reverseIntSecondSolution(-8533)).toBe(-3358);
+    });
+
+    it('Should return 0 when the input is 0', () => {
+      expect(reverseIntSecondSolution(0)).toBe(0);
+    });
   });
 });
--- algorithms/reverse-integer/index.ts
+++ algorithms/reverse-integer/index.ts
@@ -1,11 +1,19 @@
 // Given an Integer number, you need to return this number reversed and with correct signal
 
 // Solution 1
-const reverseInt = (n: number): number => {
+const reverseIntFirstSolution = (n: number): number => {
   const absoluteNumber = Math.abs(n);
   const reversed = Array.from(absoluteNumber.toString(10)).reverse().join('');
   const reversedNumber = Number(reversed);
   return n < 0 ? reversedNumber * -1 : reversedNumber;
 };
 
-export { reverseInt };
+// Solution 2 (cleaner solution)
+// no use of absolute, instead use of parseInt and Math.sign
+const reverseIntSecondSolution = (n: number): number => {
+  const reversed = Array.from(n.toString(10)).reverse().join('');
+  const reversedNumber = parseInt(reversed, 10);
+  return reversedNumber * Math.sign(n);
+};
+
+export { reverseIntFirstSolution, reverseIntSecondSolution };
