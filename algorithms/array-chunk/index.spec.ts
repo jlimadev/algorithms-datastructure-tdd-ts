e7ef56fc9f75538cbcb55fab63b03ed46fbf771f
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-08-20 18:58:10 -0300

    feat(algorithms): array chunk setup

---
+++ algorithms/array-chunk/index.spec.ts
@@ -0,0 +1,25 @@
+import { chunkArray } from '.';
+
+describe('reverse string tests', () => {
+  it('Should return right chunk array - first case', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    const chunkSize = 2;
+    const expectedResponse = [
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ];
+
+    const response = chunkArray(array, chunkSize);
+    expect(response).toEqual(expectedResponse);
+  });
+
+  it('Should return right chunk array - second case', () => {
+    const array = [1, 2, 3, 4, 5, 6, 7];
+    const chunkSize = 2;
+    const expectedResponse = [[1, 2], [3, 4], [5, 6], [7]];
+
+    const response = chunkArray(array, chunkSize);
+    expect(response).toEqual(expectedResponse);
+  });
+});
---
+++ algorithms/array-chunk/index.ts
@@ -0,0 +1,14 @@
+// Given an array and a chunk size, divide the array into many sub-arrays where each arrays is of length of chunk size
+// chunk([1, 2, 3, 4], 2) => [[1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 3) => [[1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) => [[1, 2, 3, 4, 5]]
+
+const chunkArray = (
+  array: Array<number>,
+  size: number,
+): Array<Array<number>> => {
+  const response: Array<Array<number>> = [];
+  return response;
+};
+
+export { chunkArray };
