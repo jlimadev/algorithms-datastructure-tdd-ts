0f33b3d93d7954819cf4230d788c8f16d51ef67c
Jonathan Alves de Lima <jlima.dev@gmail.com>
2021-12-11 19:11:17 -0300

    fix: change export in reverse-string

--- algorithms/reverse-string/index.ts
+++ algorithms/reverse-string/index.ts
@@ -1,8 +1,8 @@
-const reverseStringUsingArray = (str: string): string => {
+export const reverseStringUsingArray = (str: string): string => {
   return Array.from(str).reverse().join('');
 };
 
-const revereStringUsingForLoop = (str: string): string => {
+export const revereStringUsingForLoop = (str: string): string => {
   const data = [];
   for (let i = str.length - 1; i >= 0; i--) {
     data.push(str[i]);
@@ -10,14 +10,8 @@ const revereStringUsingForLoop = (str: string): string => {
   return data.join('');
 };
 
-const revereStringUsingReduce = (str: string): string => {
+export const revereStringUsingReduce = (str: string): string => {
   return Array.from(str).reduce((reversed, current) => {
     return current + reversed;
   }, '');
-};
-
-export {
-  reverseStringUsingArray,
-  revereStringUsingForLoop,
-  revereStringUsingReduce,
 };
